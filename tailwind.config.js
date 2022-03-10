6be244c768649ac657ded7a3216a11fb9417f5da
dransidu <dinitharansidhu@gmail.com>
2022-03-11 01:46:54 +0530

    Navbar customize

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#1089fe",
+                secondary: "#022144",
+            },
+        },
+    },
+    plugins: [],
+};
