df7d1b91b19470f89f4b45beb920a92830c00691
dransidu <dinitharansidhu@gmail.com>
2022-03-13 20:50:27 +0530

    Implementing dark mode on welcome

--- components/Welcome/index.jsx
+++ components/Welcome/index.jsx
@@ -6,13 +6,13 @@ export default function Welcome() {
         <div className="max-w-7xl mx-auto">
             <div className="flex flex-col sm:flex sm:flex-row p-6 items-center">
                 <div className="left">
-                    <p className="text-center sm:text-left text-gray-600 text-base">
+                    <p className="text-center sm:text-left text-gray-600 text-base dark:text-gray-200">
                         Top Web and Mobile Apps Development Company
                     </p>
-                    <p className="text-center sm:text-left text-gray-900 text-5xl sm:text-6xl font-extrabold mt-5 sm:leading-idleSize">
+                    <p className="text-center sm:text-left text-gray-900 text-5xl sm:text-6xl font-extrabold mt-5 sm:leading-idleSize dark:text-gray-100">
                         Your Trusted <br></br> Development <br></br>Partner
                     </p>
-                    <p className="text-center sm:text-left mt-5 mb-10 text-gray-600 ">
+                    <p className="text-center sm:text-left mt-5 mb-10 text-gray-600 dark:text-gray-200">
                         We deliver web and mobile app development services to
                         global businesses since 1998, with 100% project delivery
                         success. Hire the best programmers at affordable prices.
@@ -21,7 +21,7 @@ export default function Welcome() {
                     </p>
                     <a
                         href="#"
-                        className="text-center sm:text-left px-8 py-4 bg-primary mt-5 font-medium text-white rounded-lg shadow-md shadow-primary hover:shadow-lg "
+                        className="text-center sm:text-left px-8 py-4 bg-primary mt-5 font-medium text-white rounded-lg dark:shadow-md dark:shadow-primary dark:hover:shadow-lg dark:hover:shadow-primary"
                     >
                         What we do
                     </a>
