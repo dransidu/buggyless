b2331dcac4ef3188619f58022db866b769b8a33b
dransidu <dinitharansidhu@gmail.com>
2022-04-06 09:01:29 +0530

    Newsletter responsivenes implement

--- components/Newsletter/index.jsx
+++ components/Newsletter/index.jsx
@@ -3,7 +3,7 @@ export default function Newsletter() {
         <div className="">
             <div className="max-w-7xl mx-auto">
                 <div className="flex flex-col md:flex md:justify-between md:flex-row md:gap-8 bg-gray-800 dark:bg-gray-700 p-14 m-5 rounded-3xl -mt-24 items-center">
-                    <div className="md:w-5/12">
+                    <div className="md:w-5/12 w-full">
                         <h3 className="text-white font-bold text-xl md:text-3xl">
                             Sign up for our newsletter!
                         </h3>
@@ -13,7 +13,7 @@ export default function Newsletter() {
                             sign up with your email.
                         </p>
                     </div>
-                    <div className="md:w-5/12">
+                    <div className="md:w-5/12 mt-5 md:mt-0 w-full">
                         <div className="flex flex-col md:flex md:flex-row gap-2">
                             <input
                                 type="text"
