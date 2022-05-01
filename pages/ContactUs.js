1f7f8a2b0595d7c255f45990281545bc48421027
dransidu <Dinitha@DESKTOP-04DT3TB>
2022-05-01 23:14:56 +0530

    Creating Contact us page

---
+++ pages/ContactUs.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function ContactUs() {
+    return (
+        <div>
+            <Navbar/>
+            <Footer />
+        </div>
+    )
+}
