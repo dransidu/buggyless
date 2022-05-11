002a8f0cfd8223a19b60444730a112612321c3fa
dransidu <Dinitha@DESKTOP-04DT3TB>
2022-05-11 23:33:49 +0530

    Adding contactus id

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -267,6 +267,7 @@ export default function Navbar() {
                             <ThemeToggleButton/>
                         </a>
                         <a
+                            href="#contactus"
                             className="ml-8 text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-base px-6 py-2.5 text-center md:mr-5 mb-3 md:mb-0 inline-flex items-center justify-center"
                         >
                             Contact us
