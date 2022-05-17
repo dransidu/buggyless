c337f117aed668cba811167113c9ca5428811723
dransidu <Dinitha@DESKTOP-04DT3TB>
2022-05-17 07:18:12 +0530

    Change button font weight

--- components/Gurantee/index.jsx
+++ components/Gurantee/index.jsx
@@ -27,13 +27,13 @@ export default function Gurantee() {
                     </p>
                     <a
                         href="#"
-                        className="mt-5 text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-base px-6 py-2.5 text-center md:mr-5 mb-3 md:mb-0 inline-flex items-center justify-center"
+                        className="mt-5 text-white font-bold bg-primary hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-base px-6 py-2.5 text-center md:mr-5 mb-3 md:mb-0 inline-flex items-center justify-center"
                     >
                         What we do
                     </a>
                 </div>
                 <div className="sm:w-5/12">
-                    <Image src={gurantee} alt="Buggyless guarantee"/>
+                    <Image src={gurantee} alt="Buggyless guarantee" />
                 </div>
             </div>
         </div>
