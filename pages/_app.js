b7d569eae77bd595886e5adfcf873aab82e76b31
dransidu <Dinitha@DESKTOP-04DT3TB>
2022-05-17 05:43:59 +0530

    Remove unused code

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,13 @@
 import { useState } from "react";
 import "../styles/globals.css";
-import ThemeContext from "../contexts/ThemeContext";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-    const [theme, setTheme] = useState("light");
 
     return (
-        // <ThemeContext.Provider value={{ theme, setTheme }}>
         <ThemeProvider defaultTheme="system" attribute="class">
             < Component {...pageProps} />
         </ThemeProvider >
-        // </ThemeContext.Provider>
     );
 }
 
